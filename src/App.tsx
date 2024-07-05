cc8d876cf9362d3a20268fdacf7066d4b4f1e44f
Charly <cardan1990@hotmail.com>
2024-07-05 11:41:09 +0200

    Implemented voice recording and recognition

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,31 @@
 import {useState} from "react";
 
+const recognition = new window.webkitSpeechRecognition();
+
+recognition.continuous = false;
+recognition.lang = "en-US";
+
 function App() {
   const [isRecording, setIsRecording] = useState<boolean>(false);
+
+  const handleStartRecording = () => {
+    setIsRecording(true);
+
+    recognition.start();
+
+    recognition.addEventListener("result", (event) => {
+      const transcript = Array.from(event.results)
+        .map((result) => result[0])
+        .map((result) => result.transcript)
+        .join("");
+
+      console.log(transcript);
+    });
+  };
+  const handleEndRecording = () => {
+    setIsRecording(false);
+    recognition.stop();
+  };
 
   return (
     <main className="container m-auto grid min-h-screen grid-rows-[auto,1fr,auto] px-4">
@@ -9,7 +33,7 @@ function App() {
       <section className="grid place-content-center py-8">
         <button
           className={`h-64 w-64 rounded-full border-8 border-neutral-600 transition-colors ${isRecording ? "bg-red-500" : "bg-red-700"}`}
-          onClick={() => setIsRecording(!isRecording)}
+          onClick={isRecording ? handleEndRecording : handleStartRecording}
         />
       </section>
       <footer className="text-center leading-[4rem] opacity-70">
